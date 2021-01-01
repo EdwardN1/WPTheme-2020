a5ed24973c8a6e09cb8b72ac3d1020585cd6291c
Edward Nickerson <edward@technicks.com>
2021-01-01 09:10:46 +0000

    Block Editor Styles

--- assets/scripts/editor/js/editor.js
+++ assets/scripts/editor/js/editor.js
@@ -15,4 +15,26 @@ wp.domReady( () => {
             label: 'Tosca Zero Uppercase',
         }
     ]);
+
+    wp.blocks.registerBlockStyle( 'core/spacer', {
+        name: 'default',
+        label: 'Default',
+        isDefault: true,
+    });
+
+    wp.blocks.registerBlockStyle( 'core/spacer', {
+        name: 'responsive-large',
+        label: 'Responsive Large',
+    } );
+
+    wp.blocks.registerBlockStyle( 'core/spacer', {
+        name: 'responsive-medium',
+        label: 'Responsive Medium',
+    } );
+
+    wp.blocks.registerBlockStyle( 'core/spacer', {
+        name: 'responsive-small',
+        label: 'Responsive Small',
+    } );
+
 } );
