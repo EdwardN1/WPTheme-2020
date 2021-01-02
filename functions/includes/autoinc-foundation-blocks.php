05ca67a7ecc8f0c90bcf2d837a5eee8feac50285
Edward Nickerson <edward@technicks.com>
2021-01-02 08:55:43 +0000

    Foundation Orbit and Grid XY Blocks

--- functions/includes/autoinc-foundation-blocks.php
+++ functions/includes/autoinc-foundation-blocks.php
@@ -50,3 +50,36 @@ function register_foundation_orbit_block() {
     }
 
 }
+
+/**
+ *
+ * ======================= Grid XY =======================
+ *
+ */
+
+add_action( 'acf/init', 'register_foundation_xy_grid_block' );
+function register_foundation_xy_grid_block() {
+
+    if ( function_exists( 'acf_register_block_type' ) ) {
+
+        // Register foundation xy grid block
+        acf_register_block_type( array(
+            'name' 					=> 'foundation-xy-grid',
+            'title' 				=> __( 'foundation xy grid' ),
+            'description' 			=> __( 'A custom foundation xy grid block.' ),
+            'category' 				=> 'foundationblocks',
+            'icon'					=> 'layout',
+            'keywords'				=> array( 'foundation', 'xy', 'grid' ),
+            'post_types'			=> array( 'post', 'page' ),
+            'mode'					=> 'edit',
+            // 'align'				=> 'wide',
+            'render_template'		=> get_template_directory() . '/parts/blocks/BlockFoundationGridXY.php',
+            // 'render_callback'	=> 'foundation_xy_grid_block_render_callback',
+            // 'enqueue_style' 		=> get_template_directory_uri() . '/template-parts/blocks/foundation-xy-grid/foundation-xy-grid.css',
+            // 'enqueue_script' 	=> get_template_directory_uri() . '/template-parts/blocks/foundation-xy-grid/foundation-xy-grid.js',
+            // 'enqueue_assets' 	=> 'foundation_xy_grid_block_enqueue_assets',
+        ));
+
+    }
+
+}
